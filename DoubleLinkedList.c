0b7e81bbb421f5f38d2916f85dc6f78a12eb2b0c
linzhenghao233 <1392872316@qq.com>
2023-11-13 23:20:03 +0800

    1113

--- DoubleLinkedList.c
+++ DoubleLinkedList.c
@@ -6,3 +6,19 @@ typedef struct DNode {
 DLinkList* DL, * p;
 
 //双向链表的建立
+int InitDList(DLinkList* DL) {
+	DL = (DNode*)malloc(sizeof(DNode));
+	if (DL == NULL)
+		return false;
+	DL->prior = DL->next = NULL;
+
+	return true;
+}
+
+//判断双向链表
+int DlEmpty(DLinkList* DL) {
+	if (DL->prior == DL->next && DL->prior == NULL)
+		return true;
+	else
+		return false;
+}
