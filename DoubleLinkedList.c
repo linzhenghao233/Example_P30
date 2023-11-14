ca6a1f959587e725ea7da5efc035219f5f6689c9
linzhenghao233 <1392872316@qq.com>
2023-11-14 20:20:33 +0800

    1114

--- DoubleLinkedList.c
+++ DoubleLinkedList.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int ElemType;
 typedef struct DNode {
 	struct DNode* prior;
 	ElemType data;
 	struct DNode* next;
-}DLinkList;
+}DLinkList, DNode;
 DLinkList* DL, * p;
 
 //双向链表的建立
@@ -21,4 +26,50 @@ int DlEmpty(DLinkList* DL) {
 		return true;
 	else
 		return false;
+}
+
+//在双向链表中插入数据
+int DLInsert(DLinkList* DL, int i, ElemType e) {
+	int j;
+	DLinkList* temp, * node;
+	j = 1;
+	temp = DL;
+
+	while (j < i && temp->next != NULL) {
+		j++;
+		temp = temp->next;
+	}
+	if (j > i)
+		return false;
+	node = (DLinkList*)malloc(sizeof(DLinkList));
+	if (node == NULL)
+		return false;
+	node->data = e;
+	node->prior = temp;
+	node->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prior = node;
+	temp->next = node;
+
+	return true;
+}
+
+//在双向链表中删除数据
+int DLDelete(DLinkList* DL, int i) {
+	int j = 1;
+	DLinkList* temp;
+	temp = DL->next;
+
+	while (j < i && temp != NULL) {
+		j++;
+		temp = temp->next;
+	}
+	if (j < i || temp == NULL)
+		return false;
+	temp->prior->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prior = temp->prior;
+	free(temp);
+
+	return true;
 }
